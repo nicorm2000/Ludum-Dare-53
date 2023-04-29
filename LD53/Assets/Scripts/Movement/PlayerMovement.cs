09089d371a3d44b287234260e09f2ab1e2265728
Outer-Guy <66810009+Outer-Guy@users.noreply.github.com>
2023-04-29 19:07:55 -0300

    Changed player controller to add acceleration and deceleration

--- LD53/Assets/Scripts/Movement/PlayerMovement.cs
+++ LD53/Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
 namespace Movement
 {
@@ -8,10 +9,11 @@ namespace Movement
         private Rigidbody2D _rigidbody2D;
 
         private Vector2 _movementInput;
-        private Vector2 _smoothedMovementInput;
-        private Vector2 _smoothedMovementVelocity;
 
-        [SerializeField] private float speed;
+        
+        [SerializeField] private float accSpeed;
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float decSpeed;
 
         private void Awake()
         {
@@ -20,14 +22,39 @@ namespace Movement
 
         private void FixedUpdate()
         {
-            _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput,
-                ref _smoothedMovementVelocity, 0.1f);
-            _rigidbody2D.velocity = _smoothedMovementInput;
+            Vector2 _velocity = _rigidbody2D.velocity;
+            Vector2 _velocitySign = new Vector2(Math.Sign(_rigidbody2D.velocity.x), Math.Sign(_rigidbody2D.velocity.y));
+            Vector2 _movementInputSign = new Vector2(Math.Sign(_movementInput.x), Math.Sign(_movementInput.y));
+
+            // velocity is 0 if velocity is less than decspeed
+            if (_velocitySign.x * _velocity.x <= decSpeed && _movementInputSign.x == 0)
+            {
+                _velocity.x = 0;
+                _velocitySign.x = 0;
+            }
+            if (_velocitySign.y * _velocity.y <= decSpeed && _movementInputSign.y == 0)
+            {
+                _velocity.y = 0;
+                _velocitySign.y = 0;
+            }
+            // velocity is reduced if player isn´t moving in that direction
+            if (_velocitySign.x != _movementInputSign.x)
+            {
+                _velocity.x += decSpeed * -_velocitySign.x;
+            }
+            if (_velocitySign.y != _movementInputSign.y)
+            {
+                _velocity.y += decSpeed * -_velocitySign.y;
+            }
+            _velocity += _movementInput.normalized * Mathf.Clamp(_movementInput.magnitude * accSpeed, -maxSpeed, maxSpeed);
+            _rigidbody2D.velocity = _velocity;
+
+
         }
 
         private void OnMove(InputValue inputValue)
         {
-            _movementInput = inputValue.Get<Vector2>() * speed;
+            _movementInput = inputValue.Get<Vector2>().normalized;
         }
         
     }
