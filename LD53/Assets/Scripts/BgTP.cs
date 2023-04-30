2a268b20e9263d1ba0c2026051e9e0f9a04f3df8
nicorm <nicorm2000@gmail.com>
2023-04-30 19:27:48 -0300

    Added bg animation and tp

---
+++ LD53/Assets/Scripts/BgTP.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgTP : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] float distanceToTPX = 153.5f;
+    [SerializeField] float distanceToTPY = 1.5f;
+
+    Vector3 tp;
+
+    private void Start()
+    {
+        tp = new Vector3(distanceToTPX, distanceToTPY, 0f);
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (gameObject.transform.position.x <= - distanceToTPX)
+        {
+            gameObject.transform.position = tp;
+        }
+    }
+}
